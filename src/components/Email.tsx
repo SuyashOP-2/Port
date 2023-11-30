f8ed6d42b0ff716b16470e96aad492a86496a064
Suyash Shirtar <104569186+SuyashOP-2@users.noreply.github.com>
2023-11-30 22:51:07 +0530

    Update Email.tsx

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -7,7 +7,7 @@ function Email() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{
-        duration: 0.3,
+        duration: 0.4,
         ease: "easeInOut",
         delay: 1.95,
       }}
